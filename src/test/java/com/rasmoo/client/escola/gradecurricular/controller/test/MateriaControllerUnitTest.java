1d17e8901df2185bd15792b9f5b739563fd45732
Deivid <deividgabrielproc@gmail.com>
2021-05-03 15:45:13 -0300

    update: Added Unitary Test on Listing Subjects

---
+++ src/test/java/com/rasmoo/client/escola/gradecurricular/controller/test/MateriaControllerUnitTest.java
@@ -0,0 +1,62 @@
+package com.rasmoo.client.escola.gradecurricular.controller.test;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+
+import com.rasmoo.client.escola.gradecurricular.dto.MateriaDto;
+import com.rasmoo.client.escola.gradecurricular.model.Response;
+import com.rasmoo.client.escola.gradecurricular.service.IMateriaService;
+
+@ExtendWith(MockitoExtension.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@RunWith(JUnitPlatform.class)
+public class MateriaControllerUnitTest {
+
+	@LocalServerPort
+	private int port;
+
+	@MockBean
+	private IMateriaService materiaService;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	@BeforeAll
+	public static void init() {
+
+	}
+
+	@Test
+	void testListarMaterias() {
+		Mockito.when(this.materiaService.listar()).thenReturn(new ArrayList<MateriaDto>());
+
+		ResponseEntity<Response<List<MateriaDto>>> materias = restTemplate.exchange(
+				"http://localhost:" + this.port + "/materia/", HttpMethod.GET, null,
+				new ParameterizedTypeReference<Response<List<MateriaDto>>>() {
+				});
+
+		assertNotNull(materias.getBody().getData());
+		assertEquals(200, materias.getBody().getStatusCode());
+	}
+
+}
